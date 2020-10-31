026f0cc18ae756d68a6841fdd579effbeee749a3
Anone-Imouse <73369548+Anone-Imouse@users.noreply.github.com>
2020-10-31 11:36:34 -0400

    Update math.js

--- math.js
+++ math.js
@@ -42,4 +42,16 @@ function addition() {
   alert("...");
   alert(result);
   emptyp.innerHTML=("Your last result was "+result+".");
+} 
+
+function factorialize(num) {
+  var emptyp=document.getElementById("emptyp");
+  var quest=prompt("What number do you want to factorialize?");
+  
+  var result=1;
+  for (var i=1; i<=quest; i++) {
+    result*=i;
+  }
+  emptyp.innerHTML="Your last result was "+result+"."
+    console.log(result);
 }
